2fa0d631f1b011886ab82d9af5de3eb368e6c0b7
Arijit Basu <sayanarijit@gmail.com>
2022-10-12 22:16:37 +0530

    Init PdC

--- README.md
+++ README.md
@@ -1,52 +1,9 @@
-# xplr plugin template
+# tri-pane.xplr
 
-Use this template to [write your own xplr plugin](https://arijitbasu.in/xplr/en/writing-plugins.html).
+[WIP Proof of Concept) xplr plugin that implements ranger-like three pane layout support.
 
-> **NOTE:** The `src` directory is a symlink to `.` for compatibility reasons.
-> It may be removed in the future.
+### Setup
 
-## Requirements
-
-- Some tool
-
-## Installation
-
-### Install manually
-
-- Add the following line in `~/.config/xplr/init.lua`
-
-  ```lua
-  local home = os.getenv("HOME")
-  package.path = home
-    .. "/.config/xplr/plugins/?/src/init.lua;"
-    .. home
-    .. "/.config/xplr/plugins/?.lua;"
-    .. package.path
-  ```
-
-- Clone the plugin
-
-  ```bash
-  mkdir -p ~/.config/xplr/plugins
-
-  git clone https://github.com/{username}/{plugin}.xplr ~/.config/xplr/plugins/{plugin}
-  ```
-
-- Require the module in `~/.config/xplr/init.lua`
-
-  ```lua
-  require("{plugin}").setup()
-
-  -- Or
-
-  require("{plugin}").setup{
-    mode = "action",
-    key = ":",
-  }
-
-  -- Type `::` and enjoy.
-  ```
-
-## Features
-
-- Some cool feature
+```lua
+require("tri-pane").setup({ as_default_layout = true })
+```
--- init.lua
+++ init.lua
@@ -2,12 +2,210 @@
 local xplr = xplr
 ---@diagnostic enable
 
-local util = require("plugin-template1.lib.util")
+local state = {
+  listings = {},
+}
+local function quote(str)
+  return "'" .. string.gsub(str, "'", [['"'"']]) .. "'"
+end
+
+local function invert(text)
+  return "\x1b[1;7m " .. text .. " \x1b[0m"
+end
+
+local function stat(node)
+  return { node.mime_essence }
+end
+
+local function read(path, height)
+  local p = io.open(path)
+
+  if p == nil then
+    return stat(path)
+  end
+
+  local i = 0
+  local lines = {}
+  for line in p:lines() do
+    table.insert(lines, line)
+    if i == height then
+      break
+    end
+    i = i + 1
+  end
+  p:close()
+
+  return lines
+end
+
+local function dirname(filepath)
+  local is_changed = false
+  local result = filepath:gsub("/([^/]+)$", function()
+    is_changed = true
+    return ""
+  end)
+  return result, is_changed
+end
+
+local function list(path)
+  if state.listings[path] == nil then
+    local files = {}
+    local pfile = assert(io.popen("ls -a " .. quote(path)))
+    for file in pfile:lines() do
+      table.insert(files, file)
+    end
+    pfile:close()
+
+    state.listings[path] = files
+  end
+  return state.listings[path]
+end
+
+local function render_parent(ctx)
+  local parent, _ = dirname(ctx.app.pwd)
+  if parent == "/" then
+    return {}
+  elseif parent == "" then
+    return state.listings["/"] or list("/")
+  end
+  return state.listings[parent] or list(parent)
+end
+
+local function render_focus(ctx)
+  local n = ctx.app.focused_node
+
+  if n and n.canonical then
+    n = n.canonical
+  end
+
+  if n then
+    if n.is_file then
+      return read(n.absolute_path, ctx.layout_size.height)
+    elseif n.is_dir then
+      return state.listings[n.absolute_path] or list(n.absolute_path)
+    else
+      return stat(n)
+    end
+  else
+    return {}
+  end
+end
+
+local parent = {
+  CustomContent = {
+    body = {
+      DynamicList = {
+        render = "custom.tri_pane.render_parent",
+      },
+    },
+  },
+}
+
+local focus = {
+  CustomContent = {
+    body = {
+      DynamicList = {
+        render = "custom.tri_pane.render_focus",
+      },
+    },
+  },
+}
+
+local layout = {
+  Horizontal = {
+    config = {
+      constraints = {
+        { Percentage = 30 },
+        { Percentage = 40 },
+        { Percentage = 30 },
+      },
+    },
+    splits = {
+      parent,
+      "Table",
+      focus,
+    },
+  },
+}
+
+local full_layout = {
+  Vertical = {
+    config = {
+      constraints = {
+        { Length = 3 },
+        { Min = 1 },
+        { Length = 3 },
+      },
+    },
+    splits = {
+      "SortAndFilter",
+      layout,
+      "InputAndLogs",
+    },
+  },
+}
+
+local function capture(app)
+  local files = {}
+  for i, node in ipairs(app.directory_buffer.nodes) do
+    local path = node.relative_path
+    if i == app.directory_buffer.focus + 1 then
+      path = invert(path)
+    end
+    table.insert(files, path)
+  end
+
+  state.listings[app.pwd] = files
+end
+
+local function enter(app)
+  capture(app)
+  return {
+    "Enter",
+  }
+end
+
+local function back(app)
+  capture(app)
+  return {
+    "Back",
+  }
+end
 
 local function setup(args)
-  print(util.dump(args))
-  print(util.dump(xplr))
-  io.read()
+  args = args or {}
+  if args.as_default_layout == nil then
+    args.as_default_layout = true
+  end
+
+  xplr.fn.custom.tri_pane = {}
+  xplr.fn.custom.tri_pane.render_parent = render_parent
+  xplr.fn.custom.tri_pane.render_focus = render_focus
+  xplr.fn.custom.tri_pane.enter = enter
+  xplr.fn.custom.tri_pane.back = back
+
+  xplr.config.layouts.custom.tri_pane = full_layout
+
+  if args.as_default_layout then
+    xplr.config.layouts.builtin.default = full_layout
+  else
+    xplr.config.layouts.custom.tri_pane = full_layout
+    xplr.config.modes.builtin.switch_layout.key_bindings.on_key.T = {
+      help = "tri pane",
+      messages = {
+        "PopMode",
+        { SwitchLayoutCustom = "tri_pane" },
+      },
+    }
+  end
+
+  xplr.config.modes.builtin.default.key_bindings.on_key.right.messages = {
+    { CallLuaSilently = "custom.tri_pane.enter" },
+  }
+
+  xplr.config.modes.builtin.default.key_bindings.on_key.left.messages = {
+    { CallLuaSilently = "custom.tri_pane.back" },
+  }
 end
 
 return { setup = setup }
